73447a9ef70c6f24bb2241e0b076636a72d9c9bb
koki-develop <kou.pg.0131@gmail.com>
2022-07-27 14:05:29 +0900

    implemented unit test for CreateSectionWithOptions

--- todoist/sections_test.go
+++ todoist/sections_test.go
@@ -118,3 +118,61 @@ func TestClient_GetSectionsWithOptions(t *testing.T) {
 		})
 	}
 }
+
+func TestClient_CreateSectionWithOptions(t *testing.T) {
+	type args struct {
+		name      string
+		projectID int
+		opts      *CreateSectionOptions
+	}
+	tests := []struct {
+		name    string
+		args    args
+		resp    *restResponse
+		want    *Section
+		wantErr bool
+	}{
+		{
+			name: "should return section",
+			args: args{name: "SECTION", projectID: 1, opts: &CreateSectionOptions{RequestID: String("REQUEST_ID"), Order: Int(1)}},
+			resp: &restResponse{
+				StatusCode: http.StatusOK,
+				Body:       strings.NewReader(`{ "id": 1, "name": "SECTION" }`),
+			},
+			want:    &Section{ID: 1, Name: "SECTION"},
+			wantErr: false,
+		},
+		{
+			name: "should return an error if the request fails",
+			args: args{name: "SECTION", projectID: 1, opts: &CreateSectionOptions{RequestID: String("REQUEST_ID"), Order: Int(1)}},
+			resp: &restResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       strings.NewReader("ERROR_RESPONSE"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cl, api := newClientForTest()
+
+			api.On("Do", &restRequest{
+				URL:     "https://api.todoist.com/rest/v1/sections",
+				Method:  http.MethodPost,
+				Payload: map[string]interface{}{"name": tt.args.name, "project_id": tt.args.projectID, "order": *tt.args.opts.Order},
+				Headers: map[string]string{"Authorization": "Bearer TOKEN", "Content-Type": "application/json", "X-Request-Id": *tt.args.opts.RequestID},
+			}).Return(tt.resp, nil)
+
+			sec, err := cl.CreateSectionWithOptions(tt.args.name, tt.args.projectID, tt.args.opts)
+
+			assert.Equal(t, tt.want, sec)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			api.AssertExpectations(t)
+		})
+	}
+}
