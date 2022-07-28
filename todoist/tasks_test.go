fc54024b67c3948d1a14a8ee4cb454732ef0a4d5
koki-develop <kou.pg.0131@gmail.com>
2022-07-29 08:21:54 +0900

    implemented unit test for CreateTask

--- todoist/tasks_test.go
+++ todoist/tasks_test.go
@@ -189,3 +189,59 @@ func TestClient_GetTask(t *testing.T) {
 		})
 	}
 }
+
+func TestClient_CreateTask(t *testing.T) {
+	type args struct {
+		content string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		resp    *restResponse
+		want    *Task
+		wantErr bool
+	}{
+		{
+			name: "should return a task",
+			args: args{content: "TASK"},
+			resp: &restResponse{
+				StatusCode: http.StatusOK,
+				Body:       strings.NewReader(`{ "id": 1, "content": "TASK" }`),
+			},
+			want:    &Task{ID: 1, Content: "TASK"},
+			wantErr: false,
+		},
+		{
+			name: "should return an error if the request fails",
+			args: args{content: "TASK"},
+			resp: &restResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       strings.NewReader("ERROR_RESPONSE"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cl, api := newClientForTest()
+
+			api.On("Do", &restRequest{
+				URL:     "https://api.todoist.com/rest/v1/tasks",
+				Method:  http.MethodPost,
+				Payload: map[string]interface{}{"content": tt.args.content},
+				Headers: map[string]string{"Authorization": "Bearer TOKEN", "Content-Type": "application/json"},
+			}).Return(tt.resp, nil)
+
+			task, err := cl.CreateTask(tt.args.content)
+
+			assert.Equal(t, tt.want, task)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			api.AssertExpectations(t)
+		})
+	}
+}
