4750296c20bd75599337032dfdc63fba0167a3e4
koki-develop <kou.pg.0131@gmail.com>
2022-07-30 20:50:23 +0900

    implemented unit test for New

--- todoist/client_test.go
+++ todoist/client_test.go
@@ -1,6 +1,24 @@
 package todoist
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 func newClientForTest() (*Client, *mockRestAPI) {
 	api := &mockRestAPI{}
 	return &Client{token: "TOKEN", restAPI: api}, api
+}
+
+func TestNew(t *testing.T) {
+	t.Run("should return a client", func(t *testing.T) {
+		tkn := "TOKEN"
+		cl := New(tkn)
+
+		assert.NotNil(t, cl)
+		assert.NotNil(t, cl.restAPI)
+		assert.IsType(t, &Client{}, cl)
+		assert.Equal(t, tkn, cl.token)
+	})
 }
