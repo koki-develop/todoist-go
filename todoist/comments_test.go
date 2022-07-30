4d32ccec8296547acbaf3b863af6faa1778cb3a2
koki-develop <kou.pg.0131@gmail.com>
2022-07-30 14:10:53 +0900

    implemented unit test for GetTaskComments

--- todoist/comments_test.go
+++ todoist/comments_test.go
@@ -63,3 +63,58 @@ func TestClient_GetProjectComments(t *testing.T) {
 		})
 	}
 }
+
+func TestClient_GetTaskComments(t *testing.T) {
+	type args struct {
+		taskID int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		resp    *restResponse
+		want    Comments
+		wantErr bool
+	}{
+		{
+			name: "should return comments",
+			args: args{taskID: 1},
+			resp: &restResponse{
+				StatusCode: http.StatusOK,
+				Body:       strings.NewReader(`[{ "id": 1, "content": "COMMENT_1" }, { "id": 2, "content": "COMMENT_2" }]`),
+			},
+			want:    Comments{{ID: 1, Content: "COMMENT_1"}, {ID: 2, Content: "COMMENT_2"}},
+			wantErr: false,
+		},
+		{
+			name: "should return an error if the request fails",
+			args: args{taskID: 1},
+			resp: &restResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       strings.NewReader("ERROR_RESPONSE"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cl, api := newClientForTest()
+
+			api.On("Do", &restRequest{
+				URL:     fmt.Sprintf("https://api.todoist.com/rest/v1/comments?task_id=%d", tt.args.taskID),
+				Method:  http.MethodGet,
+				Headers: map[string]string{"Authorization": "Bearer TOKEN"},
+			}).Return(tt.resp, nil)
+
+			cmts, err := cl.GetTaskComments(tt.args.taskID)
+
+			assert.Equal(t, tt.want, cmts)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			api.AssertExpectations(t)
+		})
+	}
+}
