33a3f6ca13c93589ff36ef183bc1e0fa5be88e0b
koki-develop <kou.pg.0131@gmail.com>
2022-07-30 16:02:21 +0900

    implemented CreateLabel and CreateLabelWithOptions

--- todoist/labels.go
+++ todoist/labels.go
@@ -5,7 +5,8 @@ type Label struct {
 	ID int `json:"id"`
 	// Label name.
 	Name string `json:"name"`
-	// A numeric ID representing the color of the label icon. Refer to the id column in the Colors (https://developer.todoist.com/guides/#colors) guide for more info.
+	// A numeric ID representing the color of the label icon.
+	// Refer to the id column in the Colors (https://developer.todoist.com/guides/#colors) guide for more info.
 	Color int `json:"color"`
 	// Number used by clients to sort list of labels.
 	Order int `json:"order"`
@@ -24,4 +25,41 @@ func (cl *Client) GetLabels() (Labels, error) {
 	}
 
 	return labels, nil
+}
+
+// Options for creating a label.
+type CreateLabelOptions struct {
+	RequestID *string
+
+	// Label order.
+	Order *int
+	// A numeric ID representing the color of the label icon.
+	// Refer to the id column in the Colors (https://developer.todoist.com/guides/#colors) guide for more info.
+	Color *int
+	// Whether the label is a favorite (a true or false value).
+	Favorite *bool
+}
+
+// Creates a label.
+func (cl *Client) CreateLabel(name string) (*Label, error) {
+	return cl.CreateLabelWithOptions(name, nil)
+}
+
+// Creates a label with options.
+func (cl *Client) CreateLabelWithOptions(name string, opts *CreateLabelOptions) (*Label, error) {
+	p := map[string]interface{}{"name": name}
+	var reqID *string
+	if opts != nil {
+		reqID = opts.RequestID
+		addOptionalIntToMap(p, "order", opts.Order)
+		addOptionalIntToMap(p, "color", opts.Color)
+		addOptionalBoolToMap(p, "favorite", opts.Favorite)
+	}
+
+	label := Label{}
+	if err := cl.post("/v1/labels", p, reqID, &label); err != nil {
+		return nil, err
+	}
+
+	return &label, nil
 }
